e7ae2d35f7f096d55f5556a4a1da6285d1eca93c
Matthew Stevens <mstevens@cpp.edu>
2016-03-04 21:08:19 -0800

    updates

--- app/src/main/java/com/example/matthew/judex/Receiver.java
+++ app/src/main/java/com/example/matthew/judex/Receiver.java
@@ -1,15 +1,11 @@
 package com.example.matthew.judex;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
